45f83316361a801fa8dd59ccc513f1be26ceb7cb
Arpita Das <arpita1477@gmail.com>
2025-06-04 19:12:15 +0600

    do while loop

---
+++ Basicjava/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package basicjava;
+
+public class DoWhileLoop {
+    public static void main(String[] args){
+        int i=1;
+        do{
+             System.out.println(i +". Bangladesh");
+            i++;
+        }
+        while(i<=10);
+    }
+}
