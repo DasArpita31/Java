7976d883477fb9b324100f84936115371cfe1356
Arpita Das <arpita1477@gmail.com>
2025-06-09 18:31:37 +0600

    palindrome

--- string/String6.java
+++ string/String6.java
@@ -3,7 +3,7 @@ package string;
 public class String6 {
     public static void main(String[] args){
 
-        String s1 = "121";
+        String s1 = "madam";
 
         StringBuffer sb = new StringBuffer(s1);
 
