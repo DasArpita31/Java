eec3ff85f2e8c9e7ac58207778f6aef67518401c
Arpita Das <arpita1477@gmail.com>
2025-06-09 18:28:23 +0600

    methods

---
+++ string/String3.java
@@ -0,0 +1,23 @@
+package string;
+
+public class String3 {
+    public static void main(String[] args){
+        String country = "  Bangladesh is my   country";
+        System.out.println(country);
+
+        String s3 = country.trim();
+        System.out.println(s3);
+
+        char ch = country.charAt(0);
+        System.out.println("ch = "+ch);
+
+        int value = country.codePointAt(0);
+        System.out.println("value = "+value);
+
+        int pos = country.indexOf("is");
+        System.out.println("first position of n = "+pos);
+
+        pos = country.lastIndexOf('n');
+        System.out.println("last position of n ="+pos);
+    }
+}
