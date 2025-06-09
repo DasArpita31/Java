817b4551ce736ebda0d21c5bd4e0889d7b72a00f
Arpita Das <arpita1477@gmail.com>
2025-06-09 18:30:02 +0600

    palindrome

---
+++ string/String6.java
@@ -0,0 +1,18 @@
+package string;
+
+public class String6 {
+    public static void main(String[] args){
+
+        String s1 = "121";
+
+        StringBuffer sb = new StringBuffer(s1);
+
+        String s2 = sb.reverse().toString();
+        
+        if(s1.equals(s2)){
+            System.out.println("Palindrome");
+        }
+        else
+        System.out.println("Not palindrome");
+    }
+}
