461eb7b795e3764f2496f4fd8446675dcb6a4dd6
Arpita Das <arpita1477@gmail.com>
2025-06-10 13:38:53 +0600

    LocalDateTime

---
+++ oop/dateClass/DateExample3.java
@@ -0,0 +1,10 @@
+package oop.dateClass;
+
+import java.time.LocalDateTime;
+
+public class DateExample3 {
+    public static void main(String[] args) {
+        LocalDateTime datetime = LocalDateTime.now();
+        System.out.println(datetime);
+    }
+}
