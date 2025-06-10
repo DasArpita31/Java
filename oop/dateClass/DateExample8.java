87c5960cd2f28b40ee31b1be969c0a0403c38520
Arpita Das <arpita1477@gmail.com>
2025-06-10 17:38:28 +0600

    Compare

--- oop/dateClass/DateExample8.java
+++ oop/dateClass/DateExample8.java
@@ -4,10 +4,20 @@ import java.time.LocalDateTime;
 
 public class DateExample8 {
     public static void main(String[] args) {
-        LocalDateTime dateTime1 = LocalDateTime.of(2025, 06, 20, 6, 45, 05);
+        LocalDateTime dateTime1 = LocalDateTime.of(2025, 01, 1, 00, 00, 00);
         LocalDateTime dateTime2 = LocalDateTime.of(2025, 01, 1, 00, 00, 00);
 
         System.out.println(dateTime1);
         System.out.println(dateTime2);
+
+        if(dateTime1.isBefore(dateTime2)){
+            System.out.println(dateTime1+" is earlier than "+dateTime2);
+        }
+        else if(dateTime1.isAfter(dateTime2)){
+        System.out.println(dateTime1+" is later than "+dateTime2);
+        }
+        else if(dateTime1.isEqual(dateTime2)){
+        System.out.println(dateTime1+" is equal to "+dateTime2);
+        }
     }
 }
