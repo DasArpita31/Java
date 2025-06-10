af387da1a80d41e15471f4d595a130179b0d7d82
Arpita Das <arpita1477@gmail.com>
2025-06-10 13:44:18 +0600

    UTC timestamp

---
+++ oop/dateClass/DateExample4.java
@@ -0,0 +1,10 @@
+package oop.dateClass;
+
+import java.time.Instant;
+
+public class DateExample4 {
+    public static void main(String[] args) {
+        Instant instant = Instant.now();
+        System.out.println(instant);
+    }
+}
