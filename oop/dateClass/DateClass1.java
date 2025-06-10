91bd4d756ca254bf49479632c2697cd97283b33a
Arpita Das <arpita1477@gmail.com>
2025-06-10 17:43:30 +0600

    printing date

---
+++ oop/dateClass/DateClass1.java
@@ -0,0 +1,11 @@
+package oop.dateClass;
+
+import java.util.Date;
+public class DateClass1 {
+    public static void main(String[] args) {
+        
+        Date d = new Date();
+        System.out.println(d);
+        System.out.println(d.getTime());
+    }
+}
