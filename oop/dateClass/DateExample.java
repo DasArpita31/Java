efa485d8ecca36fbfd15b5fd0c41c4e88073d976
Arpita Das <arpita1477@gmail.com>
2025-06-10 13:31:27 +0600

    LocalDate

---
+++ oop/dateClass/DateExample.java
@@ -0,0 +1,11 @@
+package oop.dateClass;
+
+import java.time.LocalDate;
+
+public class DateExample {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println("Today's Date: " + date);
+    }
+}
+
