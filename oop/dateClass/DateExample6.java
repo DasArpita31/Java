7491b1468d72eb3af9954b95f29fa292605c9fc0
Arpita Das <arpita1477@gmail.com>
2025-06-10 13:56:48 +0600

    LocalDate.of()

---
+++ oop/dateClass/DateExample6.java
@@ -0,0 +1,10 @@
+package oop.dateClass;
+
+import java.time.LocalDate;
+
+public class DateExample6 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2025,06, 10);
+        System.out.println(date);
+    }
+}
