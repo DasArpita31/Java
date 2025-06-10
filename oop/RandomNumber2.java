f3c6478534ec5103d92c09357d2d9abdbc826a37
Arpita Das <arpita1477@gmail.com>
2025-06-10 18:22:11 +0600

    using Math.random()

---
+++ oop/RandomNumber2.java
@@ -0,0 +1,8 @@
+package oop;
+
+public class RandomNumber2 {
+    public static void main(String[] args) {
+        int RandomNumber = (int)(Math.random()*10)+1;
+        System.out.println(RandomNumber);
+    }
+}
