f3f2f84f836d68b0ef8add9d563bbd3fe59f623a
Arpita Das <arpita1477@gmail.com>
2025-06-12 19:22:00 +0600

    10->8

---
+++ oop/numberSystemConversion/DecimaltoOctal.java
@@ -0,0 +1,9 @@
+package oop.numberSystemConversion;
+
+public class DecimaltoOctal {
+    public static void main(String[] args) {
+    int decimal = 13;
+    String octal = Integer.toOctalString(decimal);
+    System.out.println(octal); 
+    }
+}
