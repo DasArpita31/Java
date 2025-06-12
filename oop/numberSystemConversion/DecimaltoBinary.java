512cf71da3ed18a42d23487d908c932f18c8c314
Arpita Das <arpita1477@gmail.com>
2025-06-12 19:21:22 +0600

    10->2

---
+++ oop/numberSystemConversion/DecimaltoBinary.java
@@ -0,0 +1,10 @@
+package oop.numberSystemConversion;
+
+public class DecimaltoBinary {
+    public static void main(String[] args) {
+        
+        int decimal = 3;
+        String binary = Integer.toBinaryString(decimal);
+        System.out.println(binary);
+    }
+}
