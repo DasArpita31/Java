3ec2ac8bc00a128620e570eab37a7e7683e13be0
Arpita Das <arpita1477@gmail.com>
2025-06-14 22:44:12 +0600

    String

---
+++ Basicjava/Product1.java
@@ -0,0 +1,17 @@
+package basicjava;
+
+public class Product1 {
+    public static void main(String[] args){
+        int id=101;
+        String title="iphone15";
+        String price="1895 euros";
+        String description="perfect product with best image quality";
+        String catagory="phone";
+
+        System.out.printf("Id =%d\n",id);
+        System.out.printf("Title = %s",title);
+        System.out.println("Price ="+price);
+        System.out.println("Description ="+description);
+        System.out.println("Catagory ="+catagory);
+    }
+}
