5704edb1a0fc5f3f99b073b2f92de730dabe28e7
Arpita Das <arpita1477@gmail.com>
2025-06-14 22:43:55 +0600

    printing data

---
+++ Basicjava/Product.java
@@ -0,0 +1,7 @@
+package basicjava;
+
+public class Product {
+    public static void main(String []args){
+        System.out.println("id: 101,\ntitle: iphone15,\nprice: 1895 euros,\ndescription: perfrct product with best image quality,\ncatagory: phone,");
+    }
+}
