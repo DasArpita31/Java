1a30422b5ece1d063a08e24101ff61d669bbdef4
Arpita Das <arpita1477@gmail.com>
2025-06-19 18:37:46 +0600

    printing

---
+++ oop/static_method/StaticMethodTest.java
@@ -0,0 +1,10 @@
+package oop.static_method;
+
+public class StaticMethodTest {
+    public static void main(String[] args) {
+        StaticMethod1 ob1 = new StaticMethod1();
+        ob1.display1();
+
+        StaticMethod1.display2();
+    }
+}
