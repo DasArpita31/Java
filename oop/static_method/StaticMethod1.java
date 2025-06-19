7b450b59dd975ccb4f89b310389b048b80ec9cf4
Arpita Das <arpita1477@gmail.com>
2025-06-19 18:37:01 +0600

    static and non-static

---
+++ oop/static_method/StaticMethod1.java
@@ -0,0 +1,10 @@
+package oop.static_method;
+
+public class StaticMethod1 {
+    void display1(){
+        System.out.println("I am non static method");
+    }
+    static void display2(){
+        System.out.println("I am static method");
+    }
+}
