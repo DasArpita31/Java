f3142cd739d42a578109cee0665cb4cd12c89e32
Arpita Das <arpita1477@gmail.com>
2025-06-20 12:24:38 +0600

    Variable Length Argument Object

---
+++ oop/VarargsTest.java
@@ -0,0 +1,10 @@
+package oop;
+
+public class VarargsTest {
+    public static void main(String[] args) {
+        Varargs ob =new Varargs();
+        ob.add(10, 20);
+        ob.add(10,30,40,30);
+        ob.add(30,56,35,456,345,3563,56);
+    }
+}
